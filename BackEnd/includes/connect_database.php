3626dc9b332ed52c0e50221ab60adf8b2fe9385f
mospunnavit <109953199+mospunnavit@users.noreply.github.com>
2023-09-18 22:07:32 +0700

    mosza
    555+

--- BackEnd/includes/connect_database.php
+++ BackEnd/includes/connect_database.php
@@ -6,9 +6,10 @@ $db_user = "clmonqc1u002obsmnf8f9h7my";
 $db_password = "77o93FGfV3QfYHEAuIxyMrH4";
 $db_name = "dbtesto";
 
-try{
-    $db = new PDO("mysql:host='{$db_host}';port={$db_port};dbname='{$db_name}'", $db_user, $db_password);
-    $db->setAttribute(PDO::ATTR_AUTOCOMMIT, PDO::ERRMODE_EXCEPTION);
+try {
+    // เชื่อมต่อกับ MySQL
+    $db = new PDO("mysql:host=$db_host;port=$db_port;dbname=$db_name", $db_user, $db_password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     //echo "Success!";
 }
 catch(PDOException $e){
