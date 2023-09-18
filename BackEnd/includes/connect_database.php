81597ab63a403f380e833934394a0514bc6c4e7f
mospunnavit <109953199+mospunnavit@users.noreply.github.com>
2023-09-18 22:14:53 +0700

    suck
    wow

--- BackEnd/includes/connect_database.php
+++ BackEnd/includes/connect_database.php
@@ -10,7 +10,7 @@ try {
     // เชื่อมต่อกับ MySQL
     $db = new PDO("mysql:host=$db_host;port=$db_port;dbname=$db_name", $db_user, $db_password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //echo "Success!";
+    echo "Success!";
 }
 catch(PDOException $e){
     echo "Failed to connect". $e->getMessage();
