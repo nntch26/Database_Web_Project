3c57cd6e1b0556b08b52ad4d77028df103926a3a
mospunnavit <109953199+mospunnavit@users.noreply.github.com>
2023-09-18 21:10:49 +0700

    ssss
    1454

--- BackEnd/includes/connect_database.php
+++ BackEnd/includes/connect_database.php
@@ -1,13 +1,13 @@
 <?php 
 
 $db_host = "161.246.127.24";
-$db_port = "9068";
-$db_user = "clmonqc1u002obsmnf8f9h7my";
-$db_password = "77o93FGfV3QfYHEAuIxyMrH4";
+$db_port = 3306;
+$db_user = "clmonqc1t002nbsmncwt2af9x";
+$db_password = "JpxkLX8D978E639E5qBdCwIz";
 $db_name = "dbtesto";
 
 try{
-    $db = new PDO("mysql:host={$db_host};port={$db_port};dbname={$db_name}", $db_user, $db_password);
+    $db = new PDO("mysql:host='{$db_host}';port={$db_port};dbname='{$db_name}'", $db_user, $db_password);
     $db->setAttribute(PDO::ATTR_AUTOCOMMIT, PDO::ERRMODE_EXCEPTION);
     //echo "Success!";
 }
