796b4f2ea44afb2b2176e2200ee6b6ba09c990c1
mospunnavit <109953199+mospunnavit@users.noreply.github.com>
2023-09-18 17:06:28 +0700

    testdatabase
    5555+

--- BackEnd/includes/connect_database.php
+++ BackEnd/includes/connect_database.php
@@ -1,8 +1,8 @@
 <?php 
 
-$db_host = "localhost";
-$db_user = "root";
-$db_password = "";
+$db_host = "161.246.127.24";
+$db_user = "clmonqc1t002nbsmncwt2af9x";
+$db_password = "clmonqc1t002nbsmncwt2af9x";
 $db_name = "dbtesto";
 
 
