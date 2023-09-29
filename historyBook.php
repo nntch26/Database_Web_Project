bfe6e6058b330f2bd8f46117b3ae8e1932c3b4f2
Teset13118 <109953575+Teset13118@users.noreply.github.com>
2023-09-29 22:19:52 +0700

    history bois

---
+++ historyBook.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@200;300;400;500;600&family=Noto+Sans+Thai:wght@100&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/historybookandpay.css">
+    <title>Document</title>
+ <body>
+    <table class="table">
+        <h1>History Booking</h1>
+        <h2>ประวัติการจอง</h2>
+        <thead class="table-primary">
+          <tr>
+            <th scope="col">1column</th>
+            <th scope="col">2column</th>
+            <th scope="col">3column</th>
+            <th scope="col">4column</th>
+            <th scope="col">5column</th>
+            <th scope="col">6column</th>
+            <th scope="col">7column</th>
+          </tr>
+        </thead>
+        <tbody>
+        <!--row1-->
+          <tr>
+            <td>Oh</td>
+            <td>Table</td>
+          </tr>
+        <!--row2-->
+          <tr>
+            <td>No</td>
+            <td>is</td>
+          </tr>
+        <!--row3-->
+          <tr>
+            <td>Our</td>
+            <td>broken</td>
+          </tr>
+        </tbody>
+      </table>
+      <br>
+      <button class="btn btn-primary" type="submit">ย้อนกลับ</button>
+</body>
+</html>
+
