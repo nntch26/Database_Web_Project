f28d052f5162cedb773a7520853cbda369859ff0
NTCH <117381190+nntch26@users.noreply.github.com>
2023-10-01 21:01:39 +0700

    cancle

--- BackEnd/rating_db.php
+++ BackEnd/rating_db.php
@@ -40,14 +40,26 @@ if (isset($_POST['post_com'])) {
 
 
 
+
+// ยกเลิกการจอง
 }elseif (isset($_POST['cancle_pay'])) {
 
     // เปลี่ยนสถานะของ booking
-    $sql2 = "UPDATE bookings SET bookings_status = 'Cancel' WHERE booking_id = :booking_id";
+    $sql2 = "UPDATE bookings SET bookings_status = 'Cancel Booking' WHERE booking_id = :booking_id";
   
     $upstmt2 = $db->prepare($sql2);
     $upstmt2->bindParam(':booking_id', $_SESSION["booking_id"]);
     $upstmt2->execute();
+
+
+
+    // เปลี่ยนสถานะของ payment
+    $sql3 = "UPDATE payments SET payments_status = 'Cancel Booking' 
+            WHERE booking_id = :booking_id";
+
+    $upstmt3 = $db->prepare($sql3);
+    $upstmt3->bindParam(':booking_id', $_SESSION["booking_id"]);
+    $upstmt3->execute();
 
 
 }elseif (isset($_POST['back'])){
